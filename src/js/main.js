086922eae5dc60754efc9938eece9b4a3e15b4a3
AlExpler <shelmanov33@gmail.com>
2023-08-01 15:39:22 +0300

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -8,19 +8,7 @@ var userAgent = window.navigator.userAgent,
     os = null,
     infoDiv = document.getElementById("os-info-div");
 
-if (macosPlatforms.indexOf(platform) !== -1) {
-  os = 'Mac OS';
-} else if (iosPlatforms.indexOf(platform) !== -1) {
-  os = 'iOS';
-} else if (windowsPlatforms.indexOf(platform) !== -1) {
-  os = 'Windows';
-} else if (/Android/.test(userAgent)) {
-  os = 'Android';
-} else if (!os && /Linux/.test(platform)) {
-  os = 'Linux';
-} else {
-  os = `${platform}`;
-}
+os = `${platform}`;
 
 let infoString = document.createElement('div');
 infoString.id = 'os-info-string';
